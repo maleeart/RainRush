77516888bf249ddfec78928eb34cc938511eb3b6
maleeart <maleeart@users.noreply.github.com>
2026-07-14 15:44:04 +0700

    Refresh PWA cache on updates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "rain-rush-v1";
+const CACHE = "rain-rush-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
-self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
